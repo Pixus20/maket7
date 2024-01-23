55efeda5a83aa46538c18ad16fc62dcc3db49ffc
Pixus20 <mihalcukvasa01@gmail.com>
2024-01-23 17:08:19 +0200

    create 5-th block+effects

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,17 @@ $(".slider_banner, .content_third_slider, .slider4").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 });
+//icon click
+var heartIcons = document.getElementsByClassName('heartIcon');
+var likesNumbers = document.getElementsByClassName('number');
+for (var i = 0; i < heartIcons.length; i++) {
+  heartIcons[i].addEventListener('click', function() {
+    this.classList.toggle('fa-solid');
+    var index = Array.prototype.indexOf.call(heartIcons, this);
+    if (this.classList.contains('fa-solid')) {
+      likesNumbers[index].innerText = parseInt(likesNumbers[index].innerText) + 1;
+    } else {
+      likesNumbers[index].innerText = parseInt(likesNumbers[index].innerText) - 1;
+    }
+  });
+}

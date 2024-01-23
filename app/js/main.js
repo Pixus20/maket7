363f94afb9ac8c4ce75dbd830c9008b8628d865c
Pixus20 <mihalcukvasa01@gmail.com>
2024-01-23 17:57:59 +0200

    create slider for 5-th block adaptive(not all)

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,64 @@ $(".slider_banner, .content_third_slider, .slider4").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
 });
+//slider adaptive fifth block
+function initSlickSlider() {
+  $(".fifth_images_in").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      }
+    ]
+  });
+}
+function destroySlickSlider() {
+  if ($(".fifth_images_in").hasClass("slick-initialized")) {
+    $(".fifth_images_in").slick("unslick");
+  }
+}
+$(document).ready(function() {
+  if (window.innerWidth <= 1700) {
+    initSlickSlider();
+  }
+});
+$(window).resize(function() {
+  if (window.innerWidth <= 1700) {
+    initSlickSlider();
+  } else {
+    destroySlickSlider();
+  }
+});
+
 //icon click
 var heartIcons = document.getElementsByClassName('heartIcon');
 var likesNumbers = document.getElementsByClassName('number');
